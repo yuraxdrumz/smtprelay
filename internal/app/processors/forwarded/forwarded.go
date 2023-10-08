895436e6f9d9bd15e701e61ff6e219d5ed8182af
yurik <yurik@cynet.com>
2023-10-08 16:15:56 +0300

    fix: add more endings in forwarder

--- internal/app/processors/forwarded/forwarded.go
+++ internal/app/processors/forwarded/forwarded.go
@@ -29,7 +29,7 @@ func (q *Forwarded) IsForwarded() bool {
 // TODO: see if we need to identify forward in outlook
 func (q *Forwarded) CheckForwardedStartGmail(lineString string, contentType processortypes.ContentType) (isForwarded bool) {
 	// gmail adds forwarded message
-	if strings.Contains(lineString, "---------- Forwarded message ---------") {
+	if strings.Contains(lineString, "---------- Forwarded message ---------") || strings.Contains(lineString, "Forwarded message") || strings.Contains(lineString, `<div dir="ltr" class="gmail_attr">`) {
 		logrus.Infof("hit gmail forwarded start")
 		q.isForwarded = true
 		return true
@@ -64,6 +64,14 @@ func (q *Forwarded) CheckForwardingFinishGmail(lineString string, contentType pr
 			q.isForwarded = false
 			break
 		}
+
+		brEndings := "<br><br>"
+		if strings.HasSuffix(lineString, brEndings) {
+			logrus.Infof("hit gmail forwarded end with content type: text/html")
+			q.isForwarded = false
+			break
+		}
+
 	case processortypes.TextPlain:
 		if lineString == "" {
 			logrus.Infof("hit gmail forwarded end with content type: text/plain")
