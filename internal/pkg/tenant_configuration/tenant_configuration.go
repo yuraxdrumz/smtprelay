1d0bef3d68a71a059836056fddd84fab56cbfbd0
yurik <yurik@cynet.com>
2023-10-25 11:30:07 +0300

    chore: initial commit

---
+++ internal/pkg/tenant_configuration/tenant_configuration.go
@@ -0,0 +1,16 @@
+package tenantconfiguration
+
+// Junk folder / Delete folder / drop email
+// Senders email White list / Black list
+// Recipients email White list
+// IP (Server mail) email whitelist / blacklist
+// URLs - White list / Black list
+// File Hash - White list / Black list
+// File types White list / Black list
+// Rewrite URLs to Cynet URL Safe – Yes / No
+// Show continue button – Yes / No
+// Check for malicious files – Yes / No
+// Check for malicious URLs – Yes / No
+
+type Actions interface {
+}
